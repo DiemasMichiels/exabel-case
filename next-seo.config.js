e503afda6a80b936939a08fa357624fd38f6a241
Diemas <info@diemas.dev>
2024-11-19 23:26:09 +0100

    feat: initial commit

---
+++ next-seo.config.js
@@ -0,0 +1,4 @@
+export default {
+  title: 'Exabel Case',
+  description: 'Case Study - Senior Frontend Developer - Exabel AS',
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import Page from '@components/page/Page'
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return <Page>Exabel</Page>
+}
+
+export default Index
